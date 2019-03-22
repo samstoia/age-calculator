a6919e8315faae2b61ed35846fbafd887020f513
samstoia <samstoia@gmail.com>
2019-03-22 09:30:10 -0700

    create getAge method and working test for getAge

--- spec/age-calculator-test-spec.js
+++ spec/age-calculator-test-spec.js
@@ -1,1 +1,10 @@
-// import  from "./../src/.js"
+import { getAge } from './../src/age-calculator.js';
+
+
+describe ("getAge", function() {
+  it("should return the user's age", function() {
+    let birthDate = "19871010";
+    let age = getAge(birthDate);
+    expect(age).toEqual(31)
+  });
+});
--- src/age-calculator.js
+++ src/age-calculator.js
@@ -1,1 +1,12 @@
 
+export function getAge(birthDate) {
+  let year = Number(birthDate.substr(0, 4));
+  let month = Number(birthDate.substr(4, 2)) -1;
+  let day = Number(birthDate.substr(6, 2));
+  let today = new Date();
+  let age = today.getFullYear() - year;
+  if (today.getMonth() < month || (today.getMonth() == month && today.getDate() < day)) {
+  age--;
+  }
+  return age;
+}
