051be32d7bf11e6b702307c45233af738fe66a61
samstoia <samstoia@gmail.com>
2019-03-22 11:55:11 -0700

    create methods for determining age on other planets

--- spec/age-calculator-test-spec.js
+++ spec/age-calculator-test-spec.js
@@ -1,10 +1,51 @@
-import { getAge } from './../src/age-calculator.js';
+import { getEarthAge, getMercuryAge, getVenusAge, getMarsAge, getJupiterAge } from './../src/age-calculator.js';
 
+describe ("getEarthAge", function() {
 
-describe ("getAge", function() {
   it("should return the user's age", function() {
     let birthDate = "19871010";
-    let age = getAge(birthDate);
+    let age = getEarthAge(birthDate);
     expect(age).toEqual(31)
   });
+
+});
+
+describe ("getMercuryAge", function() {
+
+  it("should return the user's mercury age", function() {
+    let birthDate = "19871010";
+    let mercuryAge = getMercuryAge(birthDate);
+    expect(mercuryAge).toEqual(129)
+  });
+
+});
+
+describe ("getVenusAge", function() {
+
+  it("should return the user's venus age", function() {
+    let birthDate = "19871010";
+    let venusAge = getVenusAge(birthDate);
+    expect(venusAge).toEqual(50)
+  });
+
+});
+
+describe ("getMarsAge", function() {
+
+  it("should return the user's mars age", function() {
+    let birthDate = "19871010";
+    let marsAge = getMarsAge(birthDate);
+    expect(marsAge).toEqual(16)
+  });
+
+});
+
+describe ("getJupiterAge", function() {
+
+  it("should return the user's jupiter age", function() {
+    let birthDate = "19871010";
+    let jupiterAge = getJupiterAge(birthDate);
+    expect(jupiterAge).toEqual(2)
+  });
+
 });
--- src/age-calculator.js
+++ src/age-calculator.js
@@ -1,5 +1,4 @@
-
-export function getAge(birthDate) {
+export function getEarthAge(birthDate) {
   let year = Number(birthDate.substr(0, 4));
   let month = Number(birthDate.substr(4, 2)) -1;
   let day = Number(birthDate.substr(6, 2));
@@ -9,4 +8,28 @@ export function getAge(birthDate) {
   age--;
   }
   return age;
+}
+
+export function getMercuryAge(birthDate) {
+  let age = getEarthAge(birthDate);
+  let mercuryAge = Math.floor(age / .24);
+  return mercuryAge;
+}
+
+export function getVenusAge(birthDate) {
+  let age = getEarthAge(birthDate);
+  let venusAge = Math.floor(age / .62);
+  return venusAge;
+}
+
+export function getMarsAge(birthDate) {
+  let age = getEarthAge(birthDate);
+  let marsAge = Math.floor(age / 1.88);
+  return marsAge;
+}
+
+export function getJupiterAge(birthDate) {
+  let age = getEarthAge(birthDate);
+  let jupiterAge = Math.floor(age / 11.86);
+  return jupiterAge;
 }
